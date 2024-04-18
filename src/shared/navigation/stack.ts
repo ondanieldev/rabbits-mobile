fd02ff060ee79b926453262f30cd5be8c0bb94d1
Daniel Nascimento <me@ondaniel.com.br>
2024-04-18 13:49:02 -0300

    feat(setup): create navigation and stack

---
+++ src/shared/navigation/stack.ts
@@ -0,0 +1,15 @@
+import { NavigationProp } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { RoutineScreenParams } from '../../features/routine/screens/RoutineScreen';
+
+export type ScreenNames = ['RoutineScreen', 'AddToRoutineScreen'];
+
+export type StackNavigationParamList = {
+  RoutineScreen: RoutineScreenParams;
+  AddToRoutineScreen: {};
+};
+
+export type StackNavigationProp = NavigationProp<StackNavigationParamList>;
+
+export const Stack = createNativeStackNavigator<StackNavigationParamList>();
