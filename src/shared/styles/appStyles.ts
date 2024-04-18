1af63e32ef51009e5d93927193f852d86c872ea5
Daniel Nascimento <me@ondaniel.com.br>
2024-04-18 13:48:15 -0300

    feat(setup): create global styles and constraints

---
+++ src/shared/styles/appStyles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from './globalStyles';
+
+export const appStyles = StyleSheet.create({
+  header: {
+    backgroundColor: colors.foreground,
+  },
+});
---
+++ src/shared/styles/globalStyles.ts
@@ -0,0 +1,34 @@
+export const spacings = {
+  xs2: 1,
+  xs: 5,
+  sm: 10,
+  md: 20,
+};
+
+export const colors = {
+  background: '#060606',
+  foreground: '#141414',
+  primary: '#51CF66',
+  primaryText: '#F8F9FA',
+  selectable: '#424242',
+  danger: '#FF6B6B',
+  disabled: '#141414',
+};
+
+export const fontSizes = {
+  xs: 7,
+  sm: 11,
+  md: 17,
+  lg: 25,
+};
+
+export const sizes = {
+  sm: 30,
+  md: 40,
+  lg: 50,
+  xl: 60,
+};
+
+export const radius = {
+  sm: 5,
+};
