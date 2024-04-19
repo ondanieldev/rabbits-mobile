d240835a6ae83aaf1e92df17e2ecf7c0bdc6d773
Daniel Nascimento <me@ondaniel.com.br>
2024-04-19 20:16:36 -0300

    feat(auth): add translations for auth feature

--- i18n.js
+++ i18n.js
@@ -2,6 +2,7 @@ import { initReactI18next } from 'react-i18next';
 
 import i18n from 'i18next';
 
+import enAuth from './public/locales/en/auth.json';
 import enCommon from './public/locales/en/common.json';
 import enRoutine from './public/locales/en/routine.json';
 
@@ -10,8 +11,9 @@ i18n.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   resources: {
     en: {
-      routine: enRoutine,
       common: enCommon,
+      auth: enAuth,
+      routine: enRoutine,
     },
   },
   lng: 'en',
