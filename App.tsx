11aec234338f0a78b11605b281a70e7b3d4b677f
Daniel Nascimento <me@ondaniel.com.br>
2024-04-20 00:25:58 -0300

    feat(api): implement auth checking

--- App.tsx
+++ App.tsx
@@ -1,42 +1,67 @@
 import './i18n';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import { Profile } from './src/features/auth/interfaces/Profile';
+import { AuthService } from './src/features/auth/services/AuthService';
 import { AuthSignInView } from './src/features/auth/views/AuthSignInView';
 import { AuthSignUpView } from './src/features/auth/views/AuthSignUpView';
 import { useRoutineEditScreen } from './src/features/routine/screens/RoutineEditScreen/use';
 import { useRoutineMainScreen } from './src/features/routine/screens/RoutineMainScreen/use';
 import { useRoutineUpsertScreen } from './src/features/routine/screens/RoutineUpsertScreen/use';
+import { useAsync } from './src/shared/hooks/useAsync';
 import { Stack } from './src/shared/navigation/stack';
 
 function App(): React.JSX.Element {
+  const {
+    fetch: readProfile,
+    isLoading: isProfileLoading,
+    output: profile,
+  } = useAsync<Profile, void>(AuthService.readProfile, {});
+
   const routineMainScreen = useRoutineMainScreen();
   const routineUpsertScreen = useRoutineUpsertScreen();
   const routineEditScreen = useRoutineEditScreen();
 
+  useEffect(() => {
+    readProfile();
+    //  eslint-disable-next-line react-hooks/exhaustive-deps -- We only want to run this once
+  }, []);
+
+  if (isProfileLoading) {
+    // TODO: Add a loading screen
+    return <></>;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          component={AuthSignInView}
-          name={'AuthSignInScreen'}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          component={AuthSignUpView}
-          name={'AuthSignUpScreen'}
-          options={{
-            headerShown: false,
-          }}
-        />
-
-        <Stack.Screen {...routineMainScreen} />
-        <Stack.Screen {...routineUpsertScreen} />
-        <Stack.Screen {...routineEditScreen} />
+        {profile === null ? (
+          <>
+            <Stack.Screen
+              component={AuthSignInView}
+              name={'AuthSignInScreen'}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              component={AuthSignUpView}
+              name={'AuthSignUpScreen'}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen {...routineMainScreen} />
+            <Stack.Screen {...routineUpsertScreen} />
+            <Stack.Screen {...routineEditScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
