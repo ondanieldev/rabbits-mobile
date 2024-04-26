2911095b8e870b5864227f954d8c6a864fe6bebb
Daniel Nascimento <me@ondaniel.com.br>
2024-04-26 16:41:31 -0300

    refactor(routine): add day offset constraint

--- src/features/routine/components/Calendar/use.ts
+++ src/features/routine/components/Calendar/use.ts
@@ -5,6 +5,8 @@ import { addWeeks, endOfWeek, startOfWeek, subWeeks } from 'date-fns';
 
 import { CalendarPops } from '.';
 
+export const calendarDayOffset = 1;
+
 export type CalendarHook = (props: CalendarPops) => {
   handleNextWeek: () => void;
   handlePrevWeek: () => void;
@@ -18,12 +20,12 @@ export const useCalendar: CalendarHook = ({
   const scrollViewRef = useRef<ScrollView>(null);
 
   const handleNextWeek = useCallback(() => {
-    setReferenceDate(startOfWeek(addWeeks(referenceDate, 1)));
+    setReferenceDate(startOfWeek(addWeeks(referenceDate, calendarDayOffset)));
     scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: false });
   }, [referenceDate, setReferenceDate]);
 
   const handlePrevWeek = useCallback(() => {
-    setReferenceDate(endOfWeek(subWeeks(referenceDate, 1)));
+    setReferenceDate(endOfWeek(subWeeks(referenceDate, calendarDayOffset)));
     scrollViewRef.current?.scrollToEnd({ animated: false });
   }, [referenceDate, setReferenceDate]);
 
