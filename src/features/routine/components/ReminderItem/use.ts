ceb3e47059954dda2b6624d9044f1ce95ec655b1
Daniel Nascimento <me@ondaniel.com.br>
2024-04-26 16:42:48 -0300

    refactor(routine): remove unused files

--- src/features/routine/components/ReminderItem/use.ts
+++
@@ -1,7 +0,0 @@
-import { ReminderItemProps } from '.';
-
-export type ReminderItemHook = (props: ReminderItemProps) => {};
-
-export const useReminderItem: ReminderItemHook = () => {
-  return {};
-};
