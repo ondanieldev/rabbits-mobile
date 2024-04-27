91390a0c20886a4e4044b1ab2171c32410d092a6
Daniel Nascimento <me@ondaniel.com.br>
2024-04-27 12:45:04 -0300

    feat(highligh-today): add button to go to today

--- src/features/routine/components/Calendar/index.tsx
+++ src/features/routine/components/Calendar/index.tsx
@@ -1,10 +1,13 @@
 import { View } from 'react-native';
 
 import { IconButton } from '../../../../shared/components/IconButton';
+import { Text } from '../../../../shared/components/Text';
+import { CalendarDay } from '../CalendarDay';
 import { CalendarWeek } from '../CalendarWeek';
 import { calendarStyles } from './calendarStyles';
 import { calendarIconProps } from './data';
 import { useCalendar } from './use';
+import { useCalendarTranslation } from './useTranslation';
 
 export interface CalendarPops {
   referenceDate: Date;
@@ -15,13 +18,24 @@ export const Calendar: React.FC<CalendarPops> = ({
   referenceDate,
   setReferenceDate,
 }) => {
-  const { handleNextWeek, handlePrevWeek, scrollViewRef } = useCalendar({
-    referenceDate,
-    setReferenceDate,
-  });
+  const { goToTodayText } = useCalendarTranslation();
+
+  const { handleNextWeek, handlePrevWeek, scrollViewRef, showGoToToday } =
+    useCalendar({
+      referenceDate,
+      setReferenceDate,
+    });
 
   return (
     <View style={calendarStyles.container}>
+      {showGoToToday === 'before' && (
+        <GoToToday
+          text={goToTodayText}
+          referenceDate={referenceDate}
+          setReferenceDate={setReferenceDate}
+        />
+      )}
+
       <IconButton
         iconProps={calendarIconProps.prev}
         buttonProps={{
@@ -41,6 +55,41 @@ export const Calendar: React.FC<CalendarPops> = ({
           onPress: handleNextWeek,
         }}
       />
+
+      {showGoToToday === 'after' && (
+        <GoToToday
+          text={goToTodayText}
+          referenceDate={referenceDate}
+          setReferenceDate={setReferenceDate}
+        />
+      )}
     </View>
+  );
+};
+
+const GoToToday = ({
+  referenceDate,
+  setReferenceDate,
+  text,
+}: CalendarPops & { text: string }) => {
+  return (
+    <CalendarDay
+      date={new Date()}
+      referenceDate={referenceDate}
+      setReferenceDate={setReferenceDate}
+      customChildren={
+        <Text
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          style={calendarStyles.textGoToToday}>
+          {text}
+        </Text>
+      }
+      styles={{
+        container: calendarStyles.containerGoToToday,
+        day: {},
+        dayOfWeek: {},
+      }}
+    />
   );
 };
