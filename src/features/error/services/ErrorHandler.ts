2019742fed79630e8334278204b09457412ba191
Daniel Nascimento <me@ondaniel.com.br>
2024-04-30 07:51:05 -0300

    feat(error): reimplement error handler

---
+++ src/features/error/services/ErrorHandler.ts
@@ -0,0 +1,32 @@
+import { AxiosError } from 'axios';
+
+import { errorCodeUnknown } from '../data/errorCodes';
+import { HabitsApiError } from '../interfaces/HabitsApiError';
+import { ObjectError } from '../interfaces/ObjectError';
+
+export type ErrorCode = string;
+
+export class ErrorHandler {
+  static handle(error: unknown) {
+    if (error instanceof AxiosError) {
+      return ErrorHandler.handleAxiosError(error);
+    }
+    if (typeof error === 'object') {
+      return ErrorHandler.handleObjectError(error as ObjectError);
+    }
+    return errorCodeUnknown;
+  }
+
+  static handleAxiosError(error: AxiosError): ErrorCode {
+    if (error.response?.data) {
+      const errorData = error.response.data as HabitsApiError;
+      const message = errorData.message;
+      return message ? message : errorCodeUnknown;
+    }
+    return errorCodeUnknown;
+  }
+
+  static handleObjectError(error: ObjectError): ErrorCode {
+    return error.message ? error.message : errorCodeUnknown;
+  }
+}
