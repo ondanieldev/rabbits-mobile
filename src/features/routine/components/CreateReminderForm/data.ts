d28eaa82a71abd0df4df0c714ddc393f4be799c4
Daniel Nascimento <me@ondaniel.com.br>
2024-05-01 14:33:19 -0300

    feat(preferences): refact reminder hook and set notification values

---
+++ src/features/routine/components/CreateReminderForm/data.ts
@@ -0,0 +1,25 @@
+import { CreateTask } from '../../interfaces/CreateTask';
+import { Task } from '../../interfaces/Task';
+import { CreateReminderSchema } from '../../schemas/createReminderSchema';
+
+export const initialValues: CreateReminderSchema = {
+  name: '',
+  daysOfWeek: [] as unknown as [number, ...number[]],
+};
+
+export const getInitialValues = (
+  ediditingReminder: Task,
+): CreateReminderSchema => ({
+  name: ediditingReminder.name,
+  daysOfWeek: ediditingReminder.daysOfWeek as unknown as [number, ...number[]],
+});
+
+export const transformData = (data: CreateReminderSchema): CreateTask => ({
+  hours: 0,
+  minutes: 0,
+  kind: 'reminder',
+  isNotificationEnabled: false,
+  isSoundEnabled: false,
+  isVibrationEnabled: false,
+  ...data,
+});
