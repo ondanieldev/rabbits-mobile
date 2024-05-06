0c09329b26726b53ace907aa850c9d344f403bad
Daniel Nascimento <me@ondaniel.com.br>
2024-05-06 15:35:36 -0300

    feat(rebrand): rename app on ios files

--- ios/habitsTests/habitsTests.m
+++ ios/habitsTests/habitsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface habitsTests : XCTestCase
+@interface rabbitsTests : XCTestCase
 
 @end
 
-@implementation habitsTests
+@implementation rabbitsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
