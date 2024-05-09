a3974ddfacfeeec059097a038fe36ba0ac5359ba
Daniel Nascimento <me@ondaniel.com.br>
2024-05-09 13:53:29 -0300

    feat(storybook): registry storybook when env has been set

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ import { AppRegistry } from 'react-native';
 import { AppWrapper } from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppWrapper);
+AppRegistry.registerComponent(appName, () => {
+  if (process.env.STORYBOOK_ENABLED === 'true') {
+    return require('./.storybook').default;
+  }
+  return AppWrapper;
+});
