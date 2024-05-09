d19365c554f2b10d22195d101b21a0481999271c
Daniel Nascimento <me@ondaniel.com.br>
2024-05-09 13:53:44 -0300

    feat(storybook): create first story

---
+++ src/shared/components/BaseTextInput/index.stories.tsx
@@ -0,0 +1,13 @@
+import { BaseTextInput } from '.';
+
+const meta = {
+  title: 'BaseTextInput',
+  component: BaseTextInput,
+};
+
+export default meta;
+
+export const BaseTextInputStory = {
+  args: {},
+  parameters: {},
+};
